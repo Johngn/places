2438bfffac55db14a37ab4675ecd7b87190958ed
John Gillan <jgillan@protonmail.com>
2022-10-09 22:03:18 +0100

    Almost done styling map

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,16 @@
+import { useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
 import Map, { Marker } from 'react-map-gl';
+import styles from '../styles/map.json';
+import layers from '../styles/layers.json';
 
 export const getServerSideProps: GetServerSideProps = async context => {
   //Fetch data from external API
   const res = await fetch('http://localhost:3000/api/places');
   const places = await res.json();
-
-  // const places = [
-  //   {
-  //     id: 1,
-  //     name: 'Dublin',
-  //     lat: 53,
-  //     lng: 6,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'London',
-  //     lat: 52,
-  //     lng: 0,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Singapore',
-  //     lat: 1,
-  //     lng: 100,
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Lagos',
-  //     lat: 6,
-  //     lng: 3,
-  //   },
-  // ];
 
   return { props: { places } };
 };
@@ -51,20 +27,45 @@ type HomeProps = {
 };
 
 const Home: NextPage<HomeProps> = ({ places }) => {
-  const addLocation = async (event: any) => {
-    const data = {
-      name: 'test',
-      lat: event.lngLat.lat,
-      lng: event.lngLat.lng,
-    };
+  const [countries, setCountries] = useState([
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'France',
+    'Ireland',
+    'Germany',
+  ]);
+  const countriesList = places.map(place => place.name);
+  console.log(countriesList);
 
-    await fetch('http://localhost:3000/api/places', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
+  const addLocation = async (event: any) => {
+    const res = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${event.lngLat.lng},${event.lngLat.lat}.json?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`
+    );
+
+    const place = await res.json();
+
+    const countryLevel = place.features.filter((addressLevel: any) => {
+      const { place_type } = addressLevel;
+      return place_type[0] === 'country';
     });
+
+    if (countryLevel.length > 0) {
+      console.log(countryLevel[0].text);
+      const data = {
+        name: countryLevel[0].text,
+        lat: event.lngLat.lat,
+        lng: event.lngLat.lng,
+      };
+
+      await fetch('http://localhost:3000/api/places', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+        },
+      });
+    }
   };
 
   return (
@@ -83,30 +84,44 @@ const Home: NextPage<HomeProps> = ({ places }) => {
         <Map
           initialViewState={{
             longitude: 0,
-            latitude: 30,
+            latitude: 0,
             zoom: 2.5,
           }}
           // style={{ width: '100%', height: '100%' }}
           mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-          mapStyle="mapbox://styles/jgillan/cl91nwdzy002u14oa9a8xrn98"
-          onMouseUp={event => addLocation(event)}
+          mapStyle={{
+            ...styles,
+            // ...layers,
+            // layers: [
+            //   {
+            //     id: 'country-boundaries (1)',
+            //     type: 'fill',
+            //     source: 'composite',
+            //     'source-layer': 'country_boundaries',
+            //     layout: {},
+            //     paint: { 'fill-color': 'hsl(100, 73%, 56%)' },
+            //   },
+            //   {
+            //     id: 'country-boundaries',
+            //     type: 'fill',
+            //     source: 'composite',
+            //     'source-layer': 'country_boundaries',
+            //     layout: {},
+            //     paint: {
+            //       'fill-color': [
+            //         'match',
+            //         ['get', 'name_en'],
+            //         countriesList,
+            //         'hsla(0, 0, 0, 0)',
+            //         'hsl(50, 73%, 56%)',
+            //       ],
+            //     },
+            //   },
+            // ],
+          }}
+          // onMouseUp={event => addLocation(event)}
           projection="globe"
-        >
-          {places.map(place => (
-            <Marker
-              longitude={place.lng}
-              latitude={place.lat}
-              draggable={true}
-              anchor={'bottom'}
-            >
-              <img
-                src="https://www.goodfreephotos.com/albums/vector-images/map-pins-vector-clipart.png"
-                width={30}
-                height={30}
-              />
-            </Marker>
-          ))}
-        </Map>
+        ></Map>
       </div>
     </div>
   );
